44651c13cf17f2b64f4bea5b708ab7221e42c383
Bhayjain <144826005+Bhayjain@users.noreply.github.com>
2024-07-20 23:03:24 +0530

    new chan api

--- reactfirst/src/components/authentication/login.jsx
+++ reactfirst/src/components/authentication/login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "http://localhost:5001/api/user/login",
+        "https://tall-a-tiv.onrender.com/api/user/login",
         { email, password },
         config
       );
