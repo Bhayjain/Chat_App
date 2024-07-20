357054b27eb6eb508945dffe36de7e50552eaaa3
Bhayjain <144826005+Bhayjain@users.noreply.github.com>
2024-07-21 00:01:34 +0530

    server link changes

--- reactfirst/src/urllink.js
+++ reactfirst/src/urllink.js
@@ -4,7 +4,7 @@
 
 
 
-export const server_link = "https://tall-a-tiv.onrender.com";
+export const server_link = "https://tall-a-tiv.onrender.com/";
 
 // export const server_link = "http://localhost:5001";
 
