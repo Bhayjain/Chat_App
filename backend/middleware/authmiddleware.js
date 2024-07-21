2626c85f081c89e08fd8fbb9d39c55cf337c6bb4
Bhayjain <144826005+Bhayjain@users.noreply.github.com>
2024-07-21 15:29:34 +0530

    sunday changes core port jwt changes new jwt

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -12,25 +12,19 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(" ")[1];
 
-      // Decodes token and verifies using the secret
+      //decodes token id
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      // Fetch user from database
       req.user = await User.findById(decoded.id).select("-password");
-
-      // If user doesn't exist
-      if (!req.user) {
-        res.status(404);
-        throw new Error("User not found");
-      }
 
       next();
     } catch (error) {
-      console.error("Token verification failed:", error.message);
       res.status(401);
       throw new Error("Not authorized, token failed");
     }
-  } else {
+  }
+
+  if (!token) {
     res.status(401);
     throw new Error("Not authorized, no token");
   }
